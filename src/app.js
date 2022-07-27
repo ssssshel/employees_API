c6bdb5b3b65afd4c0b46e94d7ed098eacba8fa14
lmnjcx <81728420+ssssshel@users.noreply.github.com>
2022-07-26 19:03:34 -0500

    act

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(cors())
 require('dotenv').config()
 app.use(morgan('dev'))
 
+app.get('/', (req, res) => { res.send('Bienvenido a la API de empleados') })
 app.use('/api/employees', require('./routes/employees.route'))
 
 const port = process.env.PORT || 3001;
